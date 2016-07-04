682fdb6cb20e4e9ed3f12f87b1adeecd05e4a2df
shukla2709 <shuklank2709@gmail.com>
2016-07-05 01:28:19 +0530

    Added wiki

--- Install/install.sh
+++ Install/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir /usr/lib/lucy
 chmod a+x lucy.sh
-cp lucy.jar /usr/lib/lucy
+cp lucy /usr/lib/lucy -r
 cp lucy.sh /usr/bin/lucy
--- Java/Lucy.java
+++ Java/Lucy.java
@@ -5,6 +5,11 @@
  */
 package lucy;
 
+import com.jaunt.*;
+import com.jaunt.component.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author naveen
@@ -13,10 +18,38 @@ public class Lucy {
 
     /**
      * @param args the command line arguments
+     * @throws com.jaunt.NotFound
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NotFound {
         // TODO code application logic here
-        System.out.print(args[0] + "\n");
+        String description = null;
+        String[] urlarray = args[0].split("\\s+");
+        String sub, rest;
+        sub = urlarray[0].substring(0, 1);
+        rest = urlarray[0].substring(1);
+        sub = sub.toUpperCase();
+        urlarray[0] = sub + rest;
+        String gotourl = urlarray[0];
+        for(int i=1; i<urlarray.length; i++){
+            sub = urlarray[i].substring(0, 1);
+            rest = urlarray[i].substring(1);
+            sub = sub.toUpperCase();
+            urlarray[i] = sub + rest;
+            gotourl = gotourl + "_" + urlarray[i];
+        }
+        if(args[0].equals("Hi!")||args[0].equals("hi!")||args[0].equals("hi")||args[0].equals("Hi")){
+            System.out.println("Hi! I am Lucy.");
+        }
+        else{
+            String url = "https://en.wikipedia.org/wiki/" + gotourl; 
+            try {
+                 UserAgent userAgent = new UserAgent();
+                 userAgent.visit(url);
+                 description = userAgent.doc.findFirst("<p>").innerText();
+                 System.out.println(description);
+            } catch (ResponseException ex) {
+                 System.out.println("Can't find " + args[0] + " Please try another keyword.");
+            }
+        }
     }
-    
 }
